e0ee939fc6bee4c6835cec7886df50ab2f9b5b23
iamcrysun <adamova.01@mail.ru>
2022-10-20 21:36:59 +0300

    Added registration page logic with some simple fields validation. If input successful it navigates to main menu page

--- app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/RegistrationFragment.kt
@@ -5,56 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.github.iamcrysun.dieordie.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RegistrationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegistrationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        val view = inflater.inflate(R.layout.fragment_registration, container, false)
+
+        view.findViewById<Button>(R.id.create_account_button).setOnClickListener {
+            if (validateRegistrationForm()) {
+                Toast
+                    .makeText(
+                        requireContext(),
+                        "Ваш аккаунт успешно создан",
+                        Toast.LENGTH_LONG
+                    )
+                    .show()
+
+                findNavController().navigate(R.id.action_registrationFragment_to_mainMenuFragment)
+            } else
+                Toast
+                    .makeText(
+                        requireContext(),
+                        "Неправильный ввод данных",
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistrationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistrationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun validateRegistrationForm(): Boolean {
+        val emailField = view?.findViewById<TextView>(R.id.email_input_edit_text)
+        val passwordField = view?.findViewById<TextView>(R.id.password_input_edit_text)
+        val passwordConfirmField =
+            view?.findViewById<TextView>(R.id.password_confirm_input_edit_text)
+
+        return !emailField?.text.isNullOrEmpty() &&
+                !passwordField?.text.isNullOrEmpty() &&
+                !passwordConfirmField?.text.isNullOrEmpty() &&
+                passwordField?.text.toString() == passwordConfirmField?.text.toString()
     }
 }
