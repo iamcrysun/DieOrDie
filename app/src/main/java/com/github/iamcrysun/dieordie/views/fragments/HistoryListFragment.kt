7a80aa37102014182172b456017b28566eec7c38
iamcrysun <83180786+iamcrysun@users.noreply.github.com>
2022-10-20 16:02:03 +0300

    Fragments moved to fragments package

--- app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/HistoryListFragment.kt
+++ app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/HistoryListFragment.kt
@@ -1,10 +1,11 @@
-package com.github.iamcrysun.dieordie
+package com.github.iamcrysun.dieordie.views.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.iamcrysun.dieordie.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DoctorsListFragment.newInstance] factory method to
+ * Use the [HistoryListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DoctorsListFragment : Fragment() {
+class HistoryListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class DoctorsListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctors_list, container, false)
+        return inflater.inflate(R.layout.fragment_history_list, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class DoctorsListFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DoctorsListFragment.
+         * @return A new instance of fragment HistoryListFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DoctorsListFragment().apply {
+            HistoryListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/LoginFragment.kt
+++ app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/LoginFragment.kt
@@ -1,10 +1,11 @@
-package com.github.iamcrysun.dieordie
+package com.github.iamcrysun.dieordie.views.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.iamcrysun.dieordie.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HistoryListFragment.newInstance] factory method to
+ * Use the [LoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HistoryListFragment : Fragment() {
+class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class HistoryListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_list, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class HistoryListFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HistoryListFragment.
+         * @return A new instance of fragment LoginFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HistoryListFragment().apply {
+            LoginFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
