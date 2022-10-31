0f3c4a40a68aecb1c325d0047324330f3ad50238
iamcrysun <83180786+iamcrysun@users.noreply.github.com>
2022-10-31 10:27:57 +0300

    Doctor class made Parceable

--- app/src/main/java/com/github/iamcrysun/dieordie/models/Doctor.kt
+++ app/src/main/java/com/github/iamcrysun/dieordie/models/Doctor.kt
@@ -1,8 +1,12 @@
 package com.github.iamcrysun.dieordie.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Doctor (
     val id: Int,
     val fullName: String,
     val specialization: String,
     val info: String,
-)
+): Parcelable
