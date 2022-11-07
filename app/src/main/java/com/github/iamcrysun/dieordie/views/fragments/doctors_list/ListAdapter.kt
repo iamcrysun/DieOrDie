27566b29d0d7a027b41a4a4e914e1a704426b104
iamcrysun <adamova.01@mail.ru>
2022-11-07 20:17:02 +0300

    Set on click listener for doctor card view item. When it is clicked, it shows detailed info about doctor

--- app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/doctors_list/ListAdapter.kt
+++ app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/doctors_list/ListAdapter.kt
@@ -3,9 +3,12 @@ package com.github.iamcrysun.dieordie.views.fragments.doctors_list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.github.iamcrysun.dieordie.R
 import com.github.iamcrysun.dieordie.models.Doctor
+import com.github.iamcrysun.dieordie.views.fragments.DoctorsDetailedFragmentDirections
 
 class ListAdapter : RecyclerView.Adapter<DoctorCardViewHolder>() {
     private var doctorsList = emptyList<Doctor>()
@@ -15,10 +18,15 @@ class ListAdapter : RecyclerView.Adapter<DoctorCardViewHolder>() {
     }
     //дата к списку
     override fun onBindViewHolder(holder: DoctorCardViewHolder, position: Int) {
-        var current = doctorsList[position]
+        val current = doctorsList[position]
 
         holder.itemView.findViewById<TextView>(R.id.doctor_name).text = current.fullName
         holder.itemView.findViewById<TextView>(R.id.doctor_info).text = current.info
+
+        holder.itemView.findViewById<ConstraintLayout>(R.id.doctor_card_item).setOnClickListener{
+            val action = DoctorsListFragmentDirections.actionDoctorsListFragmentToDoctorsDetailedFragment(current)
+            holder.itemView.findNavController().navigate(action)
+        }
     }
     //
     fun setData(doctors: List<Doctor>) {
