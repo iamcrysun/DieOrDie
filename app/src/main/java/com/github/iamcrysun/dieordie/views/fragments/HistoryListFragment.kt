151f201b2af9b59669469125fb3a59be82ecab10
iamcrysun <adamova.01@mail.ru>
2022-11-24 17:46:06 +0300

    It maps a list of appointments from the database

--- app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/HistoryListFragment.kt
+++ app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/HistoryListFragment.kt
@@ -5,26 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.iamcrysun.dieordie.R
-import com.github.iamcrysun.dieordie.models.See
+import com.github.iamcrysun.dieordie.viewmodels.SeeViewModel
 
 class HistoryListFragment : Fragment() {
+    private lateinit var seeViewModel: SeeViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_history_list, container, false)
-        //список докторов
-        val data = emptyList<See>()
-        //
+
+        seeViewModel = ViewModelProvider(this)[SeeViewModel::class.java]
+
         val adapter = ListAdapterSee()
         val seesListView = view.findViewById<RecyclerView>(R.id.sees_list_view)
         seesListView.adapter = adapter
         seesListView.layoutManager = LinearLayoutManager(requireContext())
 
-        adapter.setData(data)
+        seeViewModel.getAll.observe(viewLifecycleOwner, Observer { sees ->
+            adapter.setData(sees)
+        })
 
         return view
     }
