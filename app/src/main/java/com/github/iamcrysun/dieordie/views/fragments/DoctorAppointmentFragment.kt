f3f2bfd10d990873af1e8bbe95adf3911ba8a8a4
iamcrysun <adamova.01@mail.ru>
2022-11-24 17:46:21 +0300

    It can insert an appointment

--- app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/DoctorAppointmentFragment.kt
+++ app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/DoctorAppointmentFragment.kt
@@ -5,15 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import com.github.iamcrysun.dieordie.R
+import com.github.iamcrysun.dieordie.models.See
+import com.github.iamcrysun.dieordie.viewmodels.SeeViewModel
 
 class DoctorAppointmentFragment : Fragment() {
+    private lateinit var seeViewModel: SeeViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_doctor_appointment, container, false)
 
+        seeViewModel = ViewModelProvider(this)[SeeViewModel::class.java]
+
+        view.findViewById<Button>(R.id.making_app).setOnClickListener {
+            insertSee()
+        }
+
         return view;
+    }
+
+    private fun insertSee() {
+        val date = view?.findViewById<EditText>(R.id.datetimeapp)?.text.toString()
+
+        val see = See(0, 1, date, "Еще не прошел")
+
+        seeViewModel.addSee(see)
     }
 }
