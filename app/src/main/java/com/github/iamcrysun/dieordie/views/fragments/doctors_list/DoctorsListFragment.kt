ddc4848340a2bef7f1bb188877f9ade48d249d1c
iamcrysun <adamova.01@mail.ru>
2022-11-24 17:45:50 +0300

    It maps a list of doctors from the database

--- app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/doctors_list/DoctorsListFragment.kt
+++ app/src/main/java/com/github/iamcrysun/dieordie/views/fragments/doctors_list/DoctorsListFragment.kt
@@ -5,11 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.iamcrysun.dieordie.R
-import com.github.iamcrysun.dieordie.models.Doctor
 import com.github.iamcrysun.dieordie.viewmodels.DoctorViewModel
 
 class DoctorsListFragment : Fragment() {
@@ -23,19 +23,14 @@ class DoctorsListFragment : Fragment() {
 
         doctorViewModel = ViewModelProvider(this)[DoctorViewModel::class.java]
 
-        val doctor1 = Doctor(0, "Георгий", "Хирург", "Доктор медицинских наук")
-        val doctor2 = Doctor(0, "Анатолий", "Педиатр", "Доктор медицинских наук")
-
-        doctorViewModel.addDoctor(doctor1)
-        doctorViewModel.addDoctor(doctor2)
-
-        val data = emptyList<Doctor>()
         val adapter = ListAdapter()
         val doctorsListView = view.findViewById<RecyclerView>(R.id.doctors_list_view)
         doctorsListView.adapter = adapter
         doctorsListView.layoutManager = LinearLayoutManager(requireContext())
 
-        adapter.setData(data)
+        doctorViewModel.getAll.observe(viewLifecycleOwner, Observer { doctors ->
+            adapter.setData(doctors)
+        })
 
         return view
     }
